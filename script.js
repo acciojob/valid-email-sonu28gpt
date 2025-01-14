c07395396604c805d21a410391a0638964f525ad
sonu28gpt <98702109+sonu28gpt@users.noreply.github.com>
2025-01-14 17:29:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function validEmail(str) {
   //your JS code here.
-}
+	let pat=/^[A-Za-z0-9\._%+\-]+@[A-Za-z0-9\.\-]+\.[A-Za-z]{2,}$/;
+	return pat.test(str);
+	
+} 
 
 // Do not change the code below.
 const str = prompt("Enter an email address.");
